41d401a96dfc27c06aeb7bcce27a8fc33d051335
秃尾巴的猫 <472647301@qq.com>
2020-11-09 19:58:38 +0800

    Update ScrollAndScaleView.java

--- android/src/main/java/com/byron/kline/base/ScrollAndScaleView.java
+++ android/src/main/java/com/byron/kline/base/ScrollAndScaleView.java
@@ -72,6 +72,20 @@ public abstract class ScrollAndScaleView extends RelativeLayout implements
     }
 
     @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                getParent().requestDisallowInterceptTouchEvent(true);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                getParent().requestDisallowInterceptTouchEvent(false);
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
     public boolean onDown(MotionEvent e) {
         return false;
     }
